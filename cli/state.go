217a721b62a8da9ec0ab26553d37df878811bce3
ManoloEsS <manolo.estrada.s@gmail.com>
2025-09-19 20:49:41 -0600

    Added user handler register function and added check from database to be able to login

--- cli/state.go
+++ cli/state.go
@@ -1,7 +1,11 @@
 package cli
 
-import "github.com/ManoloEsS/gator_cli/internal/config"
+import (
+	"github.com/ManoloEsS/gator_cli/internal/config"
+	"github.com/ManoloEsS/gator_cli/internal/database"
+)
 
 type State struct {
+	Db  *database.Queries
 	Cfg *config.Config
 }
